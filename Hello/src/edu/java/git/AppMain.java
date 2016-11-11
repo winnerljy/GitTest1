ff0fbf515e5a0cf7dea46a94f418fcaf2f87267d
stu <stu@itwill>
2016-11-11 14:53:49 +0900

    아니 메세지창 어딨냐고

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -3,7 +3,11 @@ package edu.java.git;
 public class AppMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello.cho");
+		System.out.println("Hello.git");
+		System.out.println("하이 조용우");
+		System.out.println("어잌후");
+		
+		System.out.println("새로운 기능 추가");
 
 	}
 
